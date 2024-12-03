97404d8a459ae4373fffd0aa9553356c7b7dd614
Eason815 <806183233@qq.com>
2024-12-03 16:45:41 +0800

    Lab3_1

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,40 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_sub(pagetable_t pagetable, int deep){
+  // there are 2^9 = 512 PTEs in a page table.
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+
+      for(int j = 0; j <= deep; j++){
+        if(j)
+          printf(" ");
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n",i,pte,child);
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        // this PTE points to a lower-level page table.
+        uint64 child = PTE2PA(pte);
+
+        vmprint_sub((pagetable_t)child, deep + 1);
+        // pagetable[i] = 0;
+      }
+    }
+
+  }
+  // kfree((void*)pagetable);
+}
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmprint_sub(pagetable, 0);
+}
