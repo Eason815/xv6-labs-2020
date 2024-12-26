273f979635af1c89459ce5a53851d3ed7f3fdb35
Eason815 <806183233@qq.com>
2024-12-26 17:10:27 +0800

    Lab6_1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,6 +23,33 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct {
+  uint8 ref_cnt;
+  struct spinlock lock;
+} cows[(PHYSTOP - KERNBASE) >> 12];
+
+void increfcnt(uint64 pa) {
+  if (pa < KERNBASE) {
+    return;
+  }
+  pa = (pa - KERNBASE) >> 12;
+  acquire(&cows[pa].lock);
+  ++cows[pa].ref_cnt;
+  release(&cows[pa].lock);
+}
+
+uint8 decrefcnt(uint64 pa) {
+  uint8 ret;
+  if (pa < KERNBASE) {
+    return 0;
+  }
+  pa = (pa - KERNBASE) >> 12;
+  acquire(&cows[pa].lock);
+  ret = --cows[pa].ref_cnt;
+  release(&cows[pa].lock);
+  return ret;
+}
+
 void
 kinit()
 {
@@ -35,8 +62,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    increfcnt((uint64)p); 
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,6 +80,9 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  if (decrefcnt((uint64) pa)) {
+    return;
+  }
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -75,6 +107,8 @@ kalloc(void)
   if(r)
     kmem.freelist = r->next;
   release(&kmem.lock);
+  
+  increfcnt((uint64)r);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
